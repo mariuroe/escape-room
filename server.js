c498691737597cb766fe006ac784122886a01f60
ROED Marius <marius.roed@soprasteria.com>
2017-11-25 22:32:42 +0100

    fixed server.js

--- server.js
+++ server.js
@@ -16,10 +16,10 @@ const forceSSL = function() {
 
   app.use(forceSSL());
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/src'));
 
 app.listen(process.env.PORT || 8080);
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/src/index.html'));
   });
