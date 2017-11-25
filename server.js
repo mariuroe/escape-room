b2e10b7b0d84b72bb4811840af734800c51403bc
ROED Marius <marius.roed@soprasteria.com>
2017-11-25 22:43:03 +0100

    new fix in server.js

--- server.js
+++ server.js
@@ -16,10 +16,10 @@ const forceSSL = function() {
 
   app.use(forceSSL());
 
-app.use(express.static(__dirname + '/src'));
+app.use(express.static(__dirname + '/dist'));
 
 app.listen(process.env.PORT || 8080);
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/src/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
   });
