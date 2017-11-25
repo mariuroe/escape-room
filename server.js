7e8b1952fa05ae7aee9d8dfff585ee1ede608dc3
ROED Marius <marius.roed@soprasteria.com>
2017-11-25 22:28:45 +0100

    new fix in server.js

--- server.js
+++ server.js
@@ -16,10 +16,10 @@ const forceSSL = function() {
 
   app.use(forceSSL());
 
-app.use(express.static(__dirname + '/escape-room'));
+app.use(express.static(__dirname + '/dist'));
 
 app.listen(process.env.PORT || 8080);
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname, '/escape-room/src/', 'index.html'));
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
   });
